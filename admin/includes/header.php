391f5f9023dc2e96c16d0c220f093e608f940e64
zakianoor <86943025+zakianoor@users.noreply.github.com>
2022-05-17 22:28:49 +0700

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -10,7 +10,7 @@
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-        <a class="navbar-brand mr-1" href="index.php">Start Bootstrap</a>
+        <a class="navbar-brand mr-1" href="index.php">La Pâtisserie</a>
 
         <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
             <i class="fas fa-bars"></i>
@@ -24,6 +24,8 @@
                     <i class="fas fa-user-circle fa-fw"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="../index.php">Store</a>
+                    <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Settings</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
